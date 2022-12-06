cddd5fc5b3868d1a43f9751e7d57f9f98f384e2b
Nicholas Nguyen <nick.nguyen@nyu.edu>
2022-12-05 19:48:39 -0500

    feat: add api rate limiting

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+require('dotenv').config();
 
-const doodleRouter = require('./routes/doodles')
+const doodleRouter = require('./routes/doodles');
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
+
+// limit api usage to 100 requests per minute, except for whitelisted IP and URL
+const limiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 100, 
+    skip: (req) => {
+        return (req.ip === process.env.WHITELIST_IP ||
+            req.headers.referer === process.env.WHITELIST_URL);
+    }
+});
+app.use(limiter);
 
 const port = process.env.PORT || 3070;
 
-app.use('/doodles', doodleRouter)
+app.use('/doodles', doodleRouter);
 
 app.get('/', (req, res) => {
     res.send("Hello World!")
-})
+});
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
-})
+});
--- routes/doodles.js
+++ routes/doodles.js
@@ -8,20 +8,21 @@ router.route('/').get(async (req, res) => {
     const perPage = req.query.per_page || 20;
 
     const offset = (page - 1) * perPage;
-    const rangeEnd = offset + perPage
+    const rangeEnd = offset + perPage;
 
     const { data, error } = await supabase
         .from("doodles")
         .select()
         .limit(perPage)
-        .range(offset, rangeEnd)
+        .range(offset, rangeEnd);
 
     if (error) {
-        console.error(err);
-        res.status(500).send("Error when querying the doodles table.")
+        console.error(error);
+        res.status(500).send("Error when querying the doodles table.");
     }
-
-    res.json(data)
+    if (data) {
+        res.json(data);
+    }
 });
 
 router.route('/random').get(async (req, res) => {
@@ -29,32 +30,34 @@ router.route('/random').get(async (req, res) => {
         .from("random_doodle")
         .select()
         .limit(1)
-        .single()
+        .single();
 
     if (error) {
-        console.error(err);
-        res.status(500).send("Error when querying the random doodles table.")
+        console.error(error);
+        res.status(500).send("Error when querying the random doodles table.");
     }
-
-    res.json(data)
+    if (data) {
+        res.json(data);
+    }
 })
 
 router.route('/:id').get(async (req, res) => {
-    const id = req.params.id
+    const id = req.params.id;
 
     const { data, error } = await supabase
         .from("doodles")
         .select()
         .eq('id', id)
         .limit(1)
-        .single()
+        .single();
 
     if (error) {
-        console.error(err);
-        res.status(500).send("Error when querying the random doodles table.")
+        console.error(error);
+        res.status(500).send("Error when querying the doodles table.");
     }
-
-    res.json(data)
+    if (data) {
+        res.json(data);
+    }
 })
 
 module.exports = router;

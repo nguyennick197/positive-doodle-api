02f4e0f0a3e58fc49215192bf68d66dd0df44485
Nicholas Nguyen <nick.nguyen@nyu.edu>
2022-12-21 19:39:45 -0500

    feat: vercel config

--- routes/doodles.js
+++ routes/doodles.js
@@ -1,12 +1,16 @@
 const { supabase } = require("../supabase.js");
-const { doesCategoryExist } = require('../utils/categories')
-const express = require('express')
+const { doesCategoryExist } = require('../utils/categories');
+const express = require('express');
 
 const router = express.Router();
 
 router.route('/').get(async (req, res) => {
     const page = req.query.page || 1;
     const perPage = req.query.per_page || 20;
+
+    if (perPage > 100) {
+        res.status(400).send("The maximum page size is 100. Retry with a lower per_page query parameter.");
+    }
 
     const offset = (page - 1) * perPage;
     const rangeEnd = offset + perPage;
@@ -20,7 +24,7 @@ router.route('/').get(async (req, res) => {
 
         if (error) {
             console.error(error);
-            return res.status(500).send("Error when querying the doodles table.");
+            return res.status(500).json(error);
         }
         return res.json(data);
     } catch (err) {
@@ -32,58 +36,67 @@ router.route('/').get(async (req, res) => {
 router.route('/random').get(async (req, res) => {
     const category = req.query.category;
 
-    const supabaseQuery = supabase
-        .from("random_doodle")
-        .select()
-        .limit(1)
-        .single();
-
-
-    if (category) {
-        const categoryExists = doesCategoryExist(category)
-        if (!categoryExists) {
-            return res.status(500).send("This category does not exist");
+    try {
+        const supabaseQuery = supabase
+            .from("random_doodle")
+            .select()
+            .limit(1)
+            .single();
+        if (category) {
+            const categoryExists = doesCategoryExist(category)
+            if (!categoryExists) {
+                return res.status(400).send("This category does not exist");
+            }
+            supabaseQuery.eq('category', category)
         }
-        supabaseQuery.eq('category', category)
+        const { data, error } = await supabaseQuery
+        if (error) {
+            console.error(error);
+            return res.status(500).json(error);
+        }
+        return res.json(data);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
     }
-
-    const { data, error } = await supabaseQuery
-
-    if (error) {
-        console.error(error);
-        return res.status(500).send("Error when querying the random doodles table.");
-    }
-    return res.json(data);
 });
 
 router.route('/categories').get(async (req, res) => {
-    const { data, error } = await supabase
-        .from("categories")
-        .select();
-
-    if (error) {
-        console.error(error);
-        return res.status(500).send("Error getting the categories view.");
+    try {
+        const { data, error } = await supabase
+            .from("categories")
+            .select();
+        if (error) {
+            console.error(error);
+            return res.status(500).json(error);
+        }
+        return res.json(data);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
     }
-
-    return res.json(data);
 });
 
 router.route('/:id').get(async (req, res) => {
     const id = req.params.id;
 
-    const { data, error } = await supabase
-        .from("doodles")
-        .select()
-        .eq('id', id)
-        .limit(1)
-        .single();
+    try {
+        const { data, error } = await supabase
+            .from("doodles")
+            .select()
+            .eq('id', id)
+            .limit(1)
+            .single();
 
-    if (error) {
-        console.error(error);
-        return res.status(500).send("Error when querying the doodles table.");
+        if (error) {
+            console.error(error);
+            return res.status(500).json(error);
+        }
+        return res.json(data);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
     }
-    return res.json(data);
 });
 
 module.exports = router;

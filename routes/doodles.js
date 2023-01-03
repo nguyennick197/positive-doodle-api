d9c93cf1f1ecf05d20c5b2000df9a68bc9479924
Nicholas Nguyen <nick.nguyen@nyu.edu>
2023-01-02 23:15:00 -0500

    feat: replace categories with tags

--- routes/doodles.js
+++ routes/doodles.js
@@ -4,9 +4,12 @@ const express = require('express');
 
 const router = express.Router();
 
+const fieldsToGet = `id, url, created_at, tags, image_text`
+
 router.route('/').get(async (req, res) => {
     const page = req.query.page || 1;
     const perPage = req.query.per_page || 20;
+    const search = req.query.search;
 
     if (perPage > 100) {
         res.status(400).send("The maximum page size is 100. Retry with a lower per_page query parameter.");
@@ -16,12 +19,19 @@ router.route('/').get(async (req, res) => {
     const rangeEnd = offset + perPage - 1;
 
     try {
-        const { data, error } = await supabase
-            .from("doodles")
-            .select()
+        const supabaseQuery = supabase
+            .from("positive_doodles")
+            .select(fieldsToGet)
             .limit(perPage)
             .range(offset, rangeEnd)
-            .order('url', { ascending: false });
+            .order('id', { ascending: true });
+
+        if (search) {
+            let searchString = search.toLowerCase().split(" ").join(" | ");
+            supabaseQuery.textSearch('doodles_fts', searchString)
+        }
+
+        const { data, error } = await supabaseQuery;
 
         if (error) {
             console.error(error);
@@ -39,18 +49,17 @@ router.route('/random').get(async (req, res) => {
 
     try {
         const supabaseQuery = supabase
-            .from("random_doodle")
+            .from("random_positive_doodle")
             .select()
             .limit(1)
             .single();
+
         if (category) {
-            const categoryExists = doesCategoryExist(category)
-            if (!categoryExists) {
-                return res.status(400).send("This category does not exist");
-            }
-            supabaseQuery.eq('category', category)
+            let searchString = category.toLowerCase().split(" ").join(" | ");
+            supabaseQuery.textSearch('doodles_fts', searchString)
         }
-        const { data, error } = await supabaseQuery
+
+        const { data, error } = await supabaseQuery;
         if (error) {
             console.error(error);
             return res.status(500).json(error);
@@ -66,7 +75,7 @@ router.route('/categories').get(async (req, res) => {
     try {
         const { data, error } = await supabase
             .from("categories")
-            .select();
+            .select(fieldsToGet);
         if (error) {
             console.error(error);
             return res.status(500).json(error);
@@ -78,13 +87,30 @@ router.route('/categories').get(async (req, res) => {
     }
 });
 
+router.route('/tags').get(async (req, res) => {
+    try {
+        const { data, error } = await supabase
+            .from("tag_count")
+            .select(fieldsToGet);
+        if (error) {
+            console.error(error);
+            return res.status(500).json(error);
+        }
+        return res.json(data);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
+
 router.route('/:id').get(async (req, res) => {
     const id = req.params.id;
 
     try {
         const { data, error } = await supabase
-            .from("doodles")
-            .select()
+            .from("positive_doodles")
+            .select(fieldsToGet)
             .eq('id', id)
             .limit(1)
             .single();
